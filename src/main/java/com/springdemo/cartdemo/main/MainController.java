8268b09811909ffa3c9c4e0147090eba027b16ed
JangDongBin <jdb9515@naver.com>
2022-03-21 22:13:46 +0900

    gitignore update

--- src/main/java/com/springdemo/cartdemo/main/MainController.java
+++ src/main/java/com/springdemo/cartdemo/main/MainController.java
@@ -2,7 +2,7 @@ package com.springdemo.cartdemo.main;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+//import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
