4a741ec8d4f6e576b23875b60e98734b2970edeb
SuperYoplait <60890985+SuperYoplait@users.noreply.github.com>
2022-03-24 12:29:45 +0900

    폼 완성 및 디테일 페이지 구현중

--- src/main/java/com/springdemo/cartdemo/goods/GoodsController.java
+++ src/main/java/com/springdemo/cartdemo/goods/GoodsController.java
@@ -1,19 +1,26 @@
 package com.springdemo.cartdemo.goods;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/goods")
 public class GoodsController {
+
+    private final GoodsService goodsService;
+
     @GetMapping("/list")
     public String Goods_List(){
+        
         return "goods/GoodsList";
     }
 
     @GetMapping("/detail")
-    public String Goods_Detail(){
+    public String Goods_Detail(Model model, @RequestParam(required = false) Long id ){
+        goodsService.detailProcess(model, id);
         return "";
     }
 
--- src/main/java/com/springdemo/cartdemo/goods/GoodsForm.java
+++ src/main/java/com/springdemo/cartdemo/goods/GoodsForm.java
@@ -1,5 +1,31 @@
 package com.springdemo.cartdemo.goods;
 
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GoodsForm {
     
+    @NotBlank
+    private Long id;
+
+    @Length(min = 5, max = 400)
+    private String name;
+
+    private Long price;
+
+    @Length(min = 5)
+    private String context;
+
+    private String image;
+
 }
