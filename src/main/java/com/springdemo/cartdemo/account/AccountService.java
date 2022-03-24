4501fd16cf5929f2ba9e67348bb26af34708b2ce
SuperYoplait <60890985+SuperYoplait@users.noreply.github.com>
2022-03-24 16:41:50 +0900

    중간저장

--- src/main/java/com/springdemo/cartdemo/account/AccountService.java
+++ src/main/java/com/springdemo/cartdemo/account/AccountService.java
@@ -1,5 +1,14 @@
 package com.springdemo.cartdemo.account;
 
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class AccountService {
     
 }
