1687a2e710c0ed47419cd28b1211eb28d5e3a3b9
JangDongBin <jdb9515@naver.com>
2022-03-24 13:58:54 +0900

    Update GoodsController.java

--- src/main/java/com/springdemo/cartdemo/goods/GoodsController.java
+++ src/main/java/com/springdemo/cartdemo/goods/GoodsController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import lombok.RequiredArgsConstructor;
 
 @Controller
-@RequiredArgsConstructor
 @RequestMapping("/goods")
 @RequiredArgsConstructor
 public class GoodsController {
