90e649c9ac29dc859e9c08452432d207e73f98cb
JangDongBin <jdb9515@naver.com>
2022-04-05 16:17:59 +0900

    test error

--- src/test/java/com/springdemo/cartdemo/Item/ItemControllerTest.java
+++ src/test/java/com/springdemo/cartdemo/Item/ItemControllerTest.java
@@ -7,7 +7,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import java.io.File;
 import java.io.FileInputStream;
 
-//import com.springdemo.cartdemo.Item.ItemRepositroy;
+import com.springdemo.cartdemo.Item.ItemRepositroy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,38 +16,39 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-//import javax.transaction.Transactional;
+import javax.transaction.Transactional;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ItemControllerTest {
     @Autowired
     private MockMvc mockMvc;
-/*
+
     @Autowired
     private ItemRepositroy itemRepository;
     private WebApplicationContext webApplicationContext;
-*/
+
     @DisplayName("상품등록 - 정상")
     @Test
     void item_insert_true() throws Exception {
         String[] array = {"fruit" , "greens" , "milk" , "instant" , "beverage" , "seasoning" , "snacks" , "infant"};
         String fileName = "imgFile";
-        File file = new File("D:/gitproject/Spring_boot_cart/src/main/resources/static/img/test-pepe.jpg");
-
+        //File file = new File("D:/gitproject/Spring_boot_cart/src/main/resources/static/img/images.png");
+        File file = new File("/Users/macbookair/Documents/GitHub/Spring_boot_cart/src/main/resources/static/img/images.png");
+        
         MockMultipartFile image = new MockMultipartFile(fileName, new FileInputStream(file));
         
-        //mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
         for(int i = 0; i<20; i++){
             mockMvc.perform(
-                multipart("/goods/goods-add").file(image)
-                .param("name","test name" + (i+1))
-                .param("context","test context" + (i+1))
-                .param("price","100000000")
-                .param("count","1")
-                .param("categorie", array[8])
+                multipart("/item/item-add").file(image)
+                .param("name","과일 test" + (i+1))
+                .param("context","과일 상품 설명 : " + (i+1))
+                .param("price","10000")
+                .param("count","10")
+                .param("categorie", array[1])
                 .param("sole","true")
                 .with(csrf()))
                 .andExpect(status().is3xxRedirection());
