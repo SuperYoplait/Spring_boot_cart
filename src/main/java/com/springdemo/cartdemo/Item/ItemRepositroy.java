135d1015c08feb3d89c0ee029ad74014770ba84c
JangDongBin <jdb9515@naver.com>
2022-04-10 20:18:49 +0900

    Update ItemRepositroy.java

--- src/main/java/com/springdemo/cartdemo/Item/ItemRepositroy.java
+++ src/main/java/com/springdemo/cartdemo/Item/ItemRepositroy.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ItemRepositroy extends JpaRepository<Item, Long>{
     Page<Item> findByNameContaining(String name, Pageable pageable);
     Page<Item> findByCategorieContaining(String categorie, Pageable pageable);
-    //Page<Goods> findByIdContainingOrNameContainingOrderByIdDesc(String name, String name2, Pageable pageable);
 }
