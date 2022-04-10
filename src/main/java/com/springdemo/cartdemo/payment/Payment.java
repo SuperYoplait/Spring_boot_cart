91f4784ff806dc1fc84228c3e5b917238de97bd7
SuperYoplait <60890985+SuperYoplait@users.noreply.github.com>
2022-04-10 20:48:45 +0900

    결제.... 해치웠냐...?
    해치웠따!

--- src/main/java/com/springdemo/cartdemo/payment/Payment.java
+++ src/main/java/com/springdemo/cartdemo/payment/Payment.java
@@ -1,15 +1,20 @@
 package com.springdemo.cartdemo.payment;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @Entity
 @Builder
@@ -18,17 +23,21 @@ import lombok.NoArgsConstructor;
 public class Payment {
     @Id
     @GeneratedValue
-    private Long idx;
+    private UUID idx;
 
-    private Long id;
+    private Long payment_id;
 
     private Long cart_id;
 
-    private Long account_id;
-
     private Long goods_id;
 
+    private String goods_name;
+
+    private String goods_option;
+
     private Long price;
+
+    private Long cnt;
 
     private LocalDateTime time;
 
--- src/main/java/com/springdemo/cartdemo/payment/PaymentService.java
+++ src/main/java/com/springdemo/cartdemo/payment/PaymentService.java
@@ -1,10 +1,17 @@
 package com.springdemo.cartdemo.payment;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import com.springdemo.cartdemo.account.Account;
 import com.springdemo.cartdemo.account.CurrentUser;
 import com.springdemo.cartdemo.cart.CartRepositroy;
+import com.springdemo.cartdemo.cartitem.CartItem;
 import com.springdemo.cartdemo.cartitem.CartItemRepository;
 
 import org.springframework.stereotype.Service;
@@ -20,14 +27,59 @@ public class PaymentService {
 
     private final CartItemRepository cartItemRepository;
     private final CartRepositroy cartRepository;
-
+    private final PaymentRepositroy paymentRepositroy;
 
     public void paymentProcess(@CurrentUser Account account, Model model, List<String> checkedVal) {
 
+        List<Payment> newPayments = new ArrayList<Payment>();
+        List<CartItem> delCartItem = new ArrayList<CartItem>();
+        int checkedPay = 0;
+        int totalprice = 0;
+        String firstItem= null;
+        CartItem newcartitem;
         for (String idx : checkedVal) {
-        cartItemRepository.findById(Long.parseLong(idx));
-        
+            SimpleDateFormat idFormat1 = new SimpleDateFormat("yyyyMMddHHmmss");
+
+            String idFormat = idFormat1.format(System.currentTimeMillis()) + Integer.toString(((int) Math.random() * 100));
+            Payment newPaymentForm = new Payment();
+            newcartitem = cartItemRepository.findById(Long.parseLong(idx)).get();
+            
+            newPaymentForm.setIdx(UUID.randomUUID());
+            newPaymentForm.setPayment_id(Long.parseLong(idFormat));
+            newPaymentForm.setGoods_id(newcartitem.getItem().getId());
+            newPaymentForm.setCart_id(newcartitem.getCart().getId());
+            newPaymentForm.setGoods_name(newcartitem.getItem().getName());
+            newPaymentForm.setGoods_option(newcartitem.getItem().getOption());
+            newPaymentForm.setPrice(newcartitem.getItem().getPrice() * newcartitem.getCnt());
+            newPaymentForm.setCnt(newcartitem.getCnt());
+            newPaymentForm.setTime(LocalDateTime.now());
+            newPaymentForm.setState(true);
+
+            newPayments.add(newPaymentForm);
+            delCartItem.add(newcartitem);
+
+            totalprice += newPaymentForm.getPrice();
+            if (firstItem == null) {
+                firstItem = newPaymentForm.getGoods_name();
+            }
+            checkedPay++;
+        }
+
+
+        paymentRepositroy.saveAll(newPayments);
+        cartItemRepository.deleteAll(delCartItem);
+
+        if(checkedPay < 1 ) {
+            model.addAttribute("code", newPayments.get(0).getPayment_id());
+            model.addAttribute("payitem", firstItem);
+            model.addAttribute("price", totalprice);
+            model.addAttribute("itemcnt", null);
+        } else {
+            model.addAttribute("code", newPayments.get(0).getPayment_id());
+            model.addAttribute("payitem", firstItem);            
+            model.addAttribute("price", totalprice);
+            model.addAttribute("itemcnt", checkedPay - 1);
         }
     }
-    
+
 }
