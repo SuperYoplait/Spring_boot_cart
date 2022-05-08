90c3ce2ff8a6b1e37376d9cfc918b6c7113ebbf3
JangDongBin <62048762+JangDongBin@users.noreply.github.com>
2022-05-08 18:05:36 +0900

    Update AccountRoleRepository.java

--- src/main/java/com/springdemo/cartdemo/account/AccountRoleRepository.java
+++ src/main/java/com/springdemo/cartdemo/account/AccountRoleRepository.java
@@ -3,7 +3,7 @@ package com.springdemo.cartdemo.account;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
@@ -13,11 +13,7 @@ public interface AccountRoleRepository extends JpaRepository<AccountRole,Long> {
     // inner join authority auth on user.id = auth.account_id
     // inner join account_role role on role.id = auth.role_id
     // where user.nickname = 'jang';
-
     
-  /*   @Query(value = "select id from role where id = :id", nativeQuery = true)
-    List<Long> getRoles(Long id); */
-
     //체크한 권한
     List<AccountRole> findByIdIn(List<Long> roleIds);
 
